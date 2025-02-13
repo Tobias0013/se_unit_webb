258edfbb57f46057bfde1055e60db9144c2957dc
Oona Tiihonen <tiihonen.oona@gmail.com>
2025-02-13 18:06:02 +0100

    Started working on Devices page. Structured files.

---
+++ client/page/devices/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const DevicesPage = () => {
+  return (
+    <div className="devices-page">
+      <h1>Devices</h1>
+      <p>List of all available devices.</p>
+    </div>
+  );
+};
+
+export default DevicesPage;
