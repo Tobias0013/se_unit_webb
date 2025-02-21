5650434f68b29874865932c733a35794f43b4565
Oona Tiihonen <tiihonen.oona@gmail.com>
2025-02-21 22:33:01 +0100

    Added root index.tsx for all the pages

--- client/index.tsx
+++ client/index.tsx
@@ -2,38 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./style.css";
+import HomePage from "./page/home"; // WILL WE HAVE HOME PAGE OR..?
 import LoginPage from "./page/auth/login";
 import RegisterPage from "./page/auth/register";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import DevicesPage from "./page/devices/index"; // Oona's, don't touch
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const rootElem = document.getElementById("root");
-
 if (!rootElem) {
-  process.exit(1);
+  throw new Error("Root element not found");
 }
 
 const root = ReactDOM.createRoot(rootElem);
 
 root.render(
-  <>
+  <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<Navigate to="/tmp" replace={true} />} />
-        <Route path="/tmp" element={<button onClick={() => alert("Hello, react template :D")}>Click me</button>} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/register" element={<RegisterPage/>} />
-        <Route path="*" element={<Navigate to="/tmp" replace={true} />} />
+        <Route path="/" element={<HomePage />} />
+        {/* Devices = Oona's page, don't modify */}
+        <Route path="/devices" element={<DevicesPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        {/* Redirect any unknown routes to Home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
-  </>
+    <ToastContainer position="top-right" autoClose={5000} />
+  </React.StrictMode>
 );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-    <ToastContainer position="top-right" autoClose={5000} />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
