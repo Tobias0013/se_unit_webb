01869b01e08acf35d7022952ab13422b7e0da735
rosolia2 <rosol_2005@hotmail,com>
2025-02-26 18:30:19 +0100

    created MainPage

--- client/index.tsx
+++ client/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
+import MainPage from "./page/MainPage";
 
 const rootElem = document.getElementById("root");
 
@@ -8,10 +9,6 @@ if (!rootElem) {
   process.exit(1);
 }
 
-const root = ReactDOM.createRoot(rootElem);
-
-root.render(
-  <>
-    <button onClick={() => alert("Hello, react template :D")}>Click me</button>
-  </>
-);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(<MainPage />)
+;
