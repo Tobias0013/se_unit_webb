2e4ae490ad9979250549cd9d33bc928c08278417
Tobias0013 <tobbes7799@gmail.com>
2025-03-04 12:35:58 +0100

    Add header and logout route to main application

--- client/index.tsx
+++ client/index.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./style.css";
 import LoginPage from "./page/auth/login";
 import RegisterPage from "./page/auth/register";
+import Header from "./component/header/header";
+import LogOut from "./page/auth/logOut";
 
 const rootElem = document.getElementById("root");
 
@@ -16,11 +18,13 @@ const root = ReactDOM.createRoot(rootElem);
 root.render(
   <>
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route index element={<Navigate to="/tmp" replace={true} />} />
         <Route path="/tmp" element={<button onClick={() => alert("Hello, react template :D")}>Click me</button>} />
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/register" element={<RegisterPage/>} />
+        <Route path="/logout" element={<LogOut/>} />
         <Route path="*" element={<Navigate to="/tmp" replace={true} />} />
       </Routes>
     </BrowserRouter>
