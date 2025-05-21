053c2b504b45837a9881d1105fb1b586c257419d
Tobias0013 <tobbes7799@gmail.com>
2025-05-21 14:19:52 +0200

    Rename AddDevice import to AddPage for consistency with updated component structure

--- client/index.tsx
+++ client/index.tsx
@@ -13,7 +13,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./component/header/header";
 import LogOut from "./page/auth/logOut";
-import AddDevice from "./page/addDevice/addDevice";
+import AddPage from "./page/addPage/addPage";
 import NotFoundPage from "./page/404/404";
 import Error500 from "./page/500/500";
 import SchedulePage from "./page/schedule/schedule";
@@ -37,7 +37,7 @@ root.render(
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/logout" element={<LogOut />} />
-          <Route path="/addDevice" element={<AddDevice />} />
+          <Route path="/addDevice" element={<AddPage />} />
           <Route path="/schedule" element={<SchedulePage />} />
           <Route path="/500" element={<Error500 />} />
           <Route path="*" element={<NotFoundPage />} />
